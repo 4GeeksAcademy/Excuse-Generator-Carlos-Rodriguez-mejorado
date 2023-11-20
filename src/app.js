732651500bcc58c67bcab610e5e3044877c4a108
Carlos Rodriguez <139814946+CarlosRodriguez14@users.noreply.github.com>
2023-11-20 20:27:42 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,38 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector(`#btn`).addEventListener("click", () => {
+    document.querySelector(`#excuse`).innerHTML = generateExcuse();
+  });
+  console.log("Generate new excuse");
+
+  let generateExcuse = () => {
+    let who = ["The dog", "My grandma", "His turtle", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "the keys", "the car"];
+    let when = [
+      "before the class",
+      "right on time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying"
+    ];
+    let randomwho = Math.floor(Math.random() * who.length);
+    let randomaction = Math.floor(Math.random() * action.length);
+    let randomwhat = Math.floor(Math.random() * what.length);
+    let randomwhen = Math.floor(Math.random() * when.length);
+    let excuse =
+      who[randomwho] +
+      " " +
+      action[randomaction] +
+      " " +
+      what[randomwhat] +
+      " " +
+      when[randomwhen];
+
+    return excuse;
+  };
 };
